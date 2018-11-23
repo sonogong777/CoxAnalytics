3edc477be03cd34632dfdc5b15428589b5f78e25
Ken Rhee <kerhee@cisco.com>
2018-11-23 00:53:14 -0500

    complete version 0.1

--- CentralRecordingCheck.sh
+++ CentralRecordingCheck.sh
@@ -1,30 +1,51 @@
-#!/bin/bash -x
+#!/bin/bash
 #
 # cDVR Recording Report.
 # Synamedia - krhee@synamedia.com
 #
 #
+VERSION=0.1
 LIST="cox-memsql-list"
 DATE=`date +%Y%m%d`
 TMPOUT="$0.$DATE.tmp"
 TMPOUT2="$0.$DATE.tmp2"
 CSVOUT="$0.$DATE.csv"
+LOGFILE="$0.$DATE.log"
+
+exec >  >(tee -ia $LOGFILE)
+exec 2> >(tee -ia $LOGFILE >&2)
+
+echo "`date +%Y%m%d%H%M`: START $0 Script"
+if [ ! -f $LIST ];then 
+   echo "Server list file $LIST not found" >> $LOGFILE
+   exit 1
+fi
 
 #main loop 
-#for i in `cat $LIST`
-#do 
-#  scp -i $i -q RecordingCheck.sh root@$i:/root/
-#  ssh -i $i -q root@$i ./RecordingCheck.sh $i
-#done > $TMPOUT
+for i in `cat $LIST|awk '{print $1}'`
+do
+  #check ssh key 
+  if [ ! -f $i ];then
+     echo "Key file $i not found" >> $LOGFILE
+     exit 1
+  fi
 
+  #scp and execute script.
+  scp -i $i -q RecordingCheck.sh root@$i:/root/
+  ssh -i $i -q root@$i ./RecordingCheck.sh $i
+
+done > $TMPOUT
+
+echo "Data capture complete, creating csv file"
+#sort by site name
 site=($(cat $TMPOUT|awk -F"," '{print $1}'|sort|uniq))
-max=$(($(cat $TMPOUT |head -1|grep -o ,|wc -l)+1))
 
 for site in ${site[*]}
 do
-  for ((i=2; i<="$max"; i++))
-  do
-    grep $site $TMPOUT|cut -f"$i" -d,|paste -s -d, -
-  done  
+  grep $site $TMPOUT
 done > $CSVOUT
 
+#clean up tempfile
+rm -rf $TMPOUT
+
+echo "`date +%Y%m%d%H%M`: $0 COMPLETE" >> $LOGFILE
--- RecordingCheck.sh
+++ RecordingCheck.sh
@@ -30,39 +30,38 @@ done
 
 
 #main loop 
+echo -n "$SITE,Description"
 for i in $(seq $DAYS -1 1)
 do
-   echo -n "$SITE,${DAY[$i]},$SITE:Scheduled:"
-   #schedules
-   echo -n `$DBCONN "select count(*) from Recordings where ScheduledTime >= '${DAY[$i]}' AND ScheduledTime < '${DAY[$i-1]}' AND XRID like 'V%';"`
-   echo -n ",$SITE:Recordings:"
-   #Recording
-   echo -n `$DBCONN "select count(*) from Recordings where StartTime >= '${DAY[$i]}' AND StartTime < '${DAY[$i-1]}' AND XRID like 'V%';"`
-   echo -n ",$SITE:RecordingFailure:"
-   #Recording Failure
-   echo -n `$DBCONN "select count(*) from Recordings where StartTime >= '${DAY[$i]}' AND StartTime < '${DAY[$i-1]}' AND XRID like 'V%' AND ( (StatSegmentsSuccess / (StatSegmentsSuccess + StatSegmentsFailure + StatSegmentsCompleteFailure) < .98) OR (StatSegmentsSuccess =0) );"`
-   echo -n ",$SITE:AccountWithRecording:"
-   #Accounts with Recording
-   echo `$DBCONN "select count(distinct(substring(AccountID,1,20))) as 'Accounts' from Recordings where StartTime >= '${DAY[$i]}' AND StartTime < '${DAY[$i-1]}' AND XRID like 'V%';"`
+  echo -n ",${DAY[$i]}"
 done
+echo ""
 
-#printf "%4s,${DAY[1]}\n" $SITE
-##Scheduled yesterday
-##totalScheduled=$(memsql -sN -D riodev -u root -h $DBSERVER -P $DBPORT -e "select count(*) from Recordings where ScheduledTime >= '${DAY[1]}' AND ScheduledTime < '${DAY[0]}';"
-#totalScheduled=$($DBCONN "select count(*) from Recordings where ScheduledTime >= '${DAY[1]}' AND ScheduledTime < '${DAY[0]}' AND XRID like 'V%';")
-#printf "%4s,$totalScheduled,\n" $SITE
-#
-##Recordings yesterday
-##totalRecordings=$($DBCONN "select count(*) from Recordings where StartTime >= '${DAY[1]}' AND StartTime < '${DAY[0]}';")
-#totalRecordings=$($DBCONN "select count(*) from Recordings where StartTime >= '${DAY[1]}' AND StartTime < '${DAY[0]}' AND XRID like 'V%';")
-##totalFailedRec=$($DBCONN "select count(*) from Recordings where StartTime >= '${DAY[1]}' AND StartTime < '${DAY[0]}' AND ( (StatSegmentsSuccess / (StatSegmentsSuccess + StatSegmentsFailure + StatSegmentsCompleteFailure) < .98) OR (StatSegmentsSuccess =0) );")
-#totalFailedRec=$($DBCONN "select count(*) from Recordings where StartTime >= '${DAY[1]}' AND StartTime < '${DAY[0]}' AND XRID like 'V%' AND ( (StatSegmentsSuccess / (StatSegmentsSuccess + StatSegmentsFailure + StatSegmentsCompleteFailure) < .98) OR (StatSegmentsSuccess =0) );")
-#
-#printf "%4s,$totalRecordings,\n" $SITE
-#printf "%4s,$totalFailedRec,\n" $SITE
-#
-##Accounts with recordings from yestaday
-##totalAccount=$($DBCONN "select count(distinct(AccountID)) from Recordings where StartTime >= '${DAY[1]}' AND StartTime < '${DAY[0]}';")
-#totalAccount=$($DBCONN "select count(distinct(substring(AccountID,1,20))) as 'Accounts' from Recordings where StartTime >= '${DAY[1]}' AND StartTime < '${DAY[0]}' AND XRID like 'V%';")
-#
-#printf "%4s,$totalAccount\n" $SITE
+echo -n "$SITE,Scheduled"
+for i in $(seq $DAYS -1 1)
+do
+  echo -n ",`$DBCONN "select count(*) from Recordings where ScheduledTime >= '${DAY[$i]}' AND ScheduledTime < '${DAY[$i-1]}' AND XRID like 'V%';"`"
+done
+echo ""
+
+echo -n "$SITE,Recordings"
+for i in $(seq $DAYS -1 1)
+do
+  echo -n ",`$DBCONN "select count(*) from Recordings where StartTime >= '${DAY[$i]}' AND StartTime < '${DAY[$i-1]}' AND XRID like 'V%';"`"
+done
+echo ""
+
+echo -n "$SITE,RecordingFailure"
+for i in $(seq $DAYS -1 1)
+do
+  echo -n ",`$DBCONN "select count(*) from Recordings where StartTime >= '${DAY[$i]}' AND StartTime < '${DAY[$i-1]}' AND XRID like 'V%' AND ( (StatSegmentsSuccess / (StatSegmentsSuccess + StatSegmentsFailure + StatSegmentsCompleteFailure) < .98) OR (StatSegmentsSuccess =0) );"`"
+done
+echo ""
+
+echo -n "$SITE,AccountWithRecording"
+for i in $(seq $DAYS -1 1)
+do
+  echo -n ",`$DBCONN "select count(distinct(substring(AccountID,1,20))) as 'Accounts' from Recordings where StartTime >= '${DAY[$i]}' AND StartTime < '${DAY[$i-1]}' AND XRID like 'V%';"`"
+done
+echo ""
+
